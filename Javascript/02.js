ceeaade4881fc4a3759b1918205a0860e766b1b6
Aj052001 <ajsingh052001@gmail.com>
2025-09-24 19:41:08 +0530

    Day37

---
+++ Javascript/02.js
@@ -0,0 +1,26 @@
+// access by id 
+console.log(document.getElementById("box"))
+
+
+
+//access by classname
+
+console.log(document.getElementsByClassName("box"))
+
+
+
+//by qeury selector 
+
+console.log(document.querySelector(".box"))
+
+
+//by queryselector all
+
+console.log(document.querySelectorAll(".box"))
+
+//by tag name
+
+console.log(document.getElementsByTagName("h1"))
+
+
+console.log(document.querySelector(".btn"))
